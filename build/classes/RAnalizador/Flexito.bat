305649ca4f23de5b6b95821a664ac2fa45131e97
richimenendez <richimenen@gmail.com>
2020-02-04 23:35:20 -0600

    =Primer commit de laptop branch

--- build/classes/RAnalizador/Flexito.bat
+++ build/classes/RAnalizador/Flexito.bat
@@ -1,7 +1,7 @@
 SET JAVA_HOME = "C:\Program Files\Java\jdk1.8.0_111\bin"
 SET PATH = %JAVA_HOME% ; %PATH%
 SET CLASSPATH = %JAVA_HOME%;
-cd = C:\Users\Richi\OneDrive\Documents\NetBeansProjects\JavaApplication4\src\RAnalizador
-java -jar C:\Users\Richi\OneDrive\Documents\NetBeansProjects\JavaApplication4\jflex-full-1.7.0.jar Flexito.jflex 
+cd = C:\Users\Richi\Desktop\Programacion\Compi\mi-interprete\src\RAnalizador
+java -jar C:\Users\Richi\Desktop\Programacion\Compi\mi-interprete\jflex-full-1.7.0.jar Flexito.jflex 
 pause
 
--- src/RAnalizador/Sins.bat
+++ src/RAnalizador/Sins.bat
@@ -1,6 +1,6 @@
 SET JAVA_HOME = "C:\Program Files\Java\jdk1.8.0_111\bin"
 SET PATH = %JAVA_HOME% ; %PATH%
 SET CLASSPATH = %JAVA_HOME%;
-cd = C:\Users\Richi\OneDrive\Documents\NetBeansProjects\JavaApplication4\src\RAnalizador
-java -jar C:\Users\Richi\OneDrive\Documents\NetBeansProjects\JavaApplication4\java-cup-11b.jar -parser SParser -symbols SSimbolos Sins.cup
+cd = C:\Users\Richi\Desktop\Programacion\Compi\mi-interprete\src\RAnalizador
+java -jar C:\Users\Richi\Desktop\Programacion\Compi\mi-interprete\java-cup-11b.jar -parser SParser -symbols SSimbolos Sins.cup
 pause
