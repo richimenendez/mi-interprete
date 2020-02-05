6152dd3e814898e8f90a6dc3c1fb2ebde4e89b8e
richimenendez <richimenen@gmail.com>
2020-02-04 23:45:58 -0600

    Cree 2 archivos para el Flex y Cup.

---
+++ build/classes/RAnalizador/Flexito_1.bat
@@ -0,0 +1,7 @@
+SET JAVA_HOME = "C:\Program Files\Java\jdk1.8.0_111\bin"
+SET PATH = %JAVA_HOME% ; %PATH%
+SET CLASSPATH = %JAVA_HOME%;
+cd = C:\Users\Richi\Desktop\Programacion\Compi\mi-interprete\src\RAnalizador
+java -jar C:\Users\Richi\Desktop\Programacion\Compi\mi-interprete\jflex-full-1.7.0.jar Flexito.jflex 
+pause
+
